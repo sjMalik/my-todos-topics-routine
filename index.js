3cdae6ec49a7de3cadbcfea71d369c6bc6989785
sjmalik <surajit.malik@qlikchain.com>
2023-07-10 13:54:18 +0530

    code structure update, topics data update

--- index.js
+++ index.js
@@ -3,30 +3,19 @@ const expressLayouts = require('express-ejs-layouts');
 const cp = require('child_process');
 const debug = require('debug')('randomLearning:server');
 
-const topics = require('./libs/topics');
-let topicsToRead = 5;
 
 const app = express();
 
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('Welcome', {
-    topics: topics.getMultipleRandom(topicsToRead),
-    topic: topicsToRead
-  });
-});
-
-app.post('/no-of-topics', (req, res) => {
-  topicsToRead = req.body.topic;
-  res.redirect('/');
-})
+app.use('/', require('./routes/index'));
+app.use('/topics', require('./routes/topic'));
 
 app.listen(7777, () => {
   debug('running @ http://localhost:7777');
-  cp.exec('start chrome http://localhost:7777')
+  // cp.exec('start chrome http://localhost:7777')
 });
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const topics = require('../libs/topics');
+let topicsToRead = 5;
+
+router.get('/', (req, res) => {
+    res.render('Welcome', {
+        topics: topics.getMultipleRandom(topicsToRead),
+        topic: topicsToRead,
+    });
+});
+
+router.post('/no-of-topics', (req, res) => {
+    topicsToRead = req.body.topic;
+    res.redirect('/');
+});
+
+module.exports = router;
