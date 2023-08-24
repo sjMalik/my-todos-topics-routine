1a5146208c1f07e591609f80cf7773c4f0dbc2c1
sjmalik <surajit.malik@qlikchain.com>
2023-08-24 13:58:36 +0530

    some change

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const cp = require('child_process');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const debug = require('debug')('randomLearning:server');
-const app = require('./app');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+// override with POST having ?_method=DELETE or ?_method=PUT or ?_method=PATCH
+app.use(methodOverride('_method'));
+
+const db = require('./config/db.config').mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => debug('MongoDB Connected'))
+  .catch((e) => debug(e));
+
+app.use(express.static('./public'));
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/', require('./routes/index'));
+app.use('/topics', require('./routes/topic.routes'));
+app.use('/todos', require('./routes/todo.routes'));
+app.use('/routines', require('./routes/routine.routes'));
 
 app.listen(7777, () => {
   debug('running @ http://localhost:7777');
-  cp.exec('start chrome http://localhost:7777/topics.html https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox');
-  cp.exec('start chrome https://www.linkedin.com/learning/ http://localhost:7777/routines');
-  cp.exec('start chrome https://chat.openai.com/');
+  // cp.exec('start chrome http://localhost:7777/topics.html https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox');
+  // cp.exec('start chrome https://www.linkedin.com/learning/ http://localhost:7777/routines');
+  // cp.exec('start chrome https://chat.openai.com/');
 });
