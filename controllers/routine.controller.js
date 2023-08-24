f3f0195eb90b7eccdac857070fa9f488a9d3a257
sjmalik <surajit.malik@qlikchain.com>
2023-08-24 16:43:51 +0530

    routine.html

--- controllers/routine.controller.js
+++ controllers/routine.controller.js
@@ -6,8 +6,5 @@ exports.findAll = async (req, res) => {
     const now = new Date();
     const today = days[now.getDay()];
     const routines = await Routine.find({ day: today }).collation({ locale: 'en', strength: 2 }).sort({ day: 1 });
-    res.render('Routine', {
-        today,
-        routines,
-    });
+    res.send(routines);
 };
